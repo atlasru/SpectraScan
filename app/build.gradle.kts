8b2f60b06b53fb07d5dd7e9cc0b23d0bf3aa49b8
Vadim <258285264+atlasru@users.noreply.github.com>
2026-08-07 12:35:12 +0300

    Release SpectraScan 0.8.1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "com.atlas.spectrascan"
         minSdk = 29
         targetSdk = 36
-        versionCode = 12
-        versionName = "0.8.0"
+        versionCode = 13
+        versionName = "0.8.1"
     }
 
     signingConfigs {
