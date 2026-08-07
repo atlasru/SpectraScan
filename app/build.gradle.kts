efdafdd07ea0210e8cef318512949c26988c6bac
Vadim <258285264+atlasru@users.noreply.github.com>
2026-08-07 10:18:10 +0300

    Enable BuildConfig for version display

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -37,7 +37,10 @@ android {
     }
 
     kotlinOptions { jvmTarget = "17" }
-    buildFeatures { compose = true }
+    buildFeatures {
+        compose = true
+        buildConfig = true
+    }
 }
 
 dependencies {
